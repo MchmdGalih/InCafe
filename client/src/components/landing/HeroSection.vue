d8236c0200153b78c35caa82370119463f37b7bd
mochamadgalih <mochamadglh@gmail.com>
2025-05-09 00:58:06 +0700

    change UI hero section

--- client/src/components/landing/HeroSection.vue
+++ client/src/components/landing/HeroSection.vue
@@ -1,46 +1,45 @@
 <template>
-  <div
-    class="h-screen grid grid-rows-3 bg-[url('@/assets/image/coffe-bg.png')] bg-no-repeat bg-center lg:px-16 md:px-8 px-4"
-  >
-    <section class="w-full flex items-center font-secondary">
-      <div
-        class="py-4 lg:w-60 md:w-60 lg:text-center md:text-start text-center flex flex-col gap-2"
-      >
-        <h1 class="text-4xl font-extrabold">$ 9.99</h1>
-
-        <h4 class="font-light">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, possimus.
-        </h4>
-      </div>
-    </section>
-    <section class="w-full flex items-center font-primary">
-      <div class="grid grid-cols-2">
+  <div class="w-screen grid lg:grid-cols-2 gap-4 lg:py-12 py-8 px-16">
+    <section class="space-y-2 order-2 lg:order-1">
+      <div class="w-full">
         <h1
-          class="text-start font-extrabold lg:text-8xl md:text-6xl text-4xl md:-z-10 -z-10 max-w-sm"
+          class="capitalize font-secondary lg:text-6xl md:text-6xl text-2xl font-bold bg-gradient-to-r from-[#f4b572] to-[#985104] bg-clip-text text-transparent lg:text-start text-center"
         >
-          WELCOME SHOPE
-        </h1>
-        <h1 class="text-end font-extrabold lg:text-8xl md:text-6xl text-4xl md:-z-10 -z-10">
-          TO COFFEE INCAFE
+          Fuel Your Day, One Cup at a Time
         </h1>
       </div>
-    </section>
-    <section class="w-full flex lg:flex-row md:flex-row flex-col items-center py-4">
-      <div
-        class="lg:w-56 w-full p-4 flex flex-col lg:bg-white md:bg-white bg-white/50 backdrop-blur-sm rounded-2xl text-center gap-2"
-      >
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, dignissimos.</h1>
-        <div
-          class="p-2 rounded-md flex justify-center items-center gap-2 cursor-pointer hover:bg-amber-900 hover:text-white transition-all duration-500 ease-in-out shadow-md shadow-amber-950"
+
+      <div class="lg:py-4 py-2 lg:text-start text-center">
+        <p class="lg:text-2xl md:text-md font-secondary font-light">
+          Elevete your productivity & build your mood with a glass of coffee in the morning & we're
+          always ready to serve with absolutely the best quality coffee.
+        </p>
+      </div>
+      <div class="space-x-4 font-secondary flex lg:justify-start justify-center items-center">
+        <button
+          class="border text-black py-2 px-8 rounded-full shadow-2xl cursor-pointer hover:bg-amber-900 hover:text-white transition-all duration-500 ease-in-out"
         >
-          <button class="cursor-pointer font-bold">ORDER NOW</button>
-          <span class="p-2 text-center">
-            <font-awesome-icon icon="fa-solid fa-arrow-right " size="xl"></font-awesome-icon>
-          </span>
-        </div>
+          Login
+        </button>
+        <button
+          class="bg-soft-white py-2 px-4 rounded-full cursor-pointer hover:bg-amber-900 hover:text-white transition-all duration-500 ease-in-out"
+        >
+          Order Now
+        </button>
+      </div>
+    </section>
+    <section
+      class="relative order-1 lg:order-2 lg:col-start-2 col-start-1 flex justify-center items-center"
+    >
+      <div>
+        <article class="lg:w-96 lg:h-80 md:w-72 md:h-72 w-48 h-48">
+          <img :src="coffee" alt="coffee" class="w-full h-full object-cover" />
+        </article>
       </div>
     </section>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import coffee from '@/assets/image/coffe-bg.png'
+</script>
